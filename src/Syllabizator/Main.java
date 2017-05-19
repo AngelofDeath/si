341bb8a22fb7ba8c297140ed12e48e5da4224593
krupatomasz <tomaszkrupa39@gmail.com>
2017-05-20 01:44:35 +0200

    Update Main.java
    Generating words after adding Bayes' net.

--- src/Syllabizator/Main.java
+++ src/Syllabizator/Main.java
@@ -6,6 +6,12 @@
 package Syllabizator;
 
 import static Syllabizator.Syllabizator.syllabize;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,16 +22,37 @@ public class Main
     public static void main(String[] args)
     {
         Net net = new Net();
-        String[] tmp = syllabize("jarosław","PL").split("-");
-        net.add(tmp[0], null);
-        net.add(tmp[1], tmp[0]);
-        net.add(tmp[1], tmp[0]);
-        net.add(tmp[1], tmp[0]);
-        net.add(tmp[1], tmp[0]);
-        net.add(tmp[2], tmp[0]);
-        net.add(tmp[2], tmp[0]);
-        net.add(tmp[2], tmp[0]);
-        net.add(tmp[2], tmp[1]);
-        return;
+        String[] tmp;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("slowa.txt"));
+            String word = br.readLine();
+            while(word != null){
+                tmp = syllabize(word, "PL").split("-");
+                for (int i=0; i<tmp.length; i++){
+                    if(i == 0)
+                        net.add(tmp[0], null);
+                    else
+                        net.add(tmp[i], tmp[i-1]);
+                }
+                word = br.readLine();
+            }
+            br.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        net.setUnconditionalProbs();
+        net.setConditionalProbs();
+        for(int i=0; i<100; i++){
+            Node tmp2 = net.getFirstSyllable();
+            System.out.print(tmp2.getSyllable());
+            tmp2 = tmp2.nextNode();
+            System.out.print(tmp2.getSyllable());
+            tmp2 = tmp2.nextNode();
+            System.out.print(tmp2.getSyllable());
+            System.out.println();
+        }
     }
 }
