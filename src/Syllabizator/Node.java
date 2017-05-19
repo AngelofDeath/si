5a48af8ac05f231b64133cfa971b631d610bb852
krupatomasz <tomaszkrupa39@gmail.com>
2017-05-20 01:46:35 +0200

    Update Node.java
    added probabilities and next node generator based on them

--- src/Syllabizator/Node.java
+++ src/Syllabizator/Node.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  *
@@ -16,22 +17,23 @@ import java.util.Map;
  */
 public class Node
 {
-    //TODO: add probability
-    private Map <Node,Integer> neighbors;
+    private Map <Node,Double> neighbors;
     private List <Node> parents;
     private String syllable;
-    private int sum;
+    private int sum, nStartingNodes;
+    private double startingNodeCDF; //unconditional probability
     public Node(String syllable)
     {
         this.syllable = syllable;
-        this.neighbors = new HashMap<Node,Integer>();
+        this.neighbors = new HashMap<Node,Double>();
         this.parents = new ArrayList<Node>();
         this.sum = 0;
+        this.nStartingNodes = 0; 
     }
     
     public void add(Node node)
     {
-        for (Map.Entry<Node, Integer> entry : this.neighbors.entrySet()) 
+        for (Map.Entry<Node, Double> entry : this.neighbors.entrySet()) 
         {
             Node key = entry.getKey();
             if(key.getSyllable().equals(node.getSyllable()))
@@ -43,7 +45,7 @@ public class Node
         }
         node.addParent(this);
         this.sum += 1;
-        this.neighbors.put(node, new Integer(1));
+        this.neighbors.put(node, 1.0);
     }
     
     public String getSyllable()
@@ -61,5 +63,44 @@ public class Node
             }
         }
         this.parents.add(node);
+    }
+    
+    public void incrementCounter(){
+        this.nStartingNodes++;
+    }
+    
+    public double getStartingNodeCDF(){
+        return startingNodeCDF;
+    }
+    
+    public void setStartingNodeCDF(double prob){
+        startingNodeCDF = prob;
+    }
+    
+    public int getNStartingNodes(){
+        return nStartingNodes;
+    }
+    
+    public void setConditionalProbs(){ // changes value in neighbours map from neighbour counter to cdf value
+        double distribution = 0, tmp;
+        for (Map.Entry<Node, Double> entry : this.neighbors.entrySet()) {
+            tmp = entry.getValue() / sum;
+            distribution += tmp;
+            entry.setValue(distribution);
+        }
+    }
+    
+    public Node nextNode(){ // can only be run after setConditionalProbs!
+        Random gen = new Random();
+        double rand = gen.nextDouble();
+        Node last = null;
+        for (Map.Entry<Node, Double> entry : this.neighbors.entrySet()) {
+            if(rand <= entry.getValue())
+                return entry.getKey();
+            last = entry.getKey();
+        }
+        if(last == null)
+            last = new Node("pie");
+        return last;
     }
 }
